ae78abbb1cbaac0c0a9056d7967225a3683ef20c
Benjamin Weis <bweis@techfak.de>
2025-05-08 18:07:49 +0200

    Main angepasst um nur 2 Bilder zu verarbeiten

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from coordinates.coordinates_memory import CoordinateBuffer 
 from steuerung.environment import Environment
+from load_camera_action import load_camera_action
+from steuerung.UDP_connector import UDPConnector
+
+
 
 
 if __name__ == "__main__":
 
     coordinateBuffer = CoordinateBuffer()
     environment = Environment()
+    load_camera_action()
     print(environment.goal_boundaries)
-    
+    print(coordinateBuffer.latest(2))
+    #print(x,y)
+    positions = load_camera_action()
+    print(positions)
+    #UDPConnector.send_coordinates(x, y)
