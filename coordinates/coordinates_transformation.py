b2ab02acb26e4941da9b9aea1959abf5067f3c0d
leonakoester <looneylo33@gmailcom>
2025-05-08 16:15:03 +0200

    min/max

--- coordinates/coordinates_transformation.py
+++ coordinates/coordinates_transformation.py
@@ -1,4 +1,4 @@
 def calc_robot_coordinates(x, y):
-    x_robot = (x - 290)/728  * 450 + 10
-    y_robot = (y - 164)/800  * 450 + 10
+    x_robot = (x - 290)/728  * 431 + 10
+    y_robot = (y - 164)/800  * 470 + 10
     return x_robot, y_robot
