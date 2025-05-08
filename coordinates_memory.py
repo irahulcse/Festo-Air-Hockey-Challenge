29d8043559eb89654d015c045e773cb5c2a772af
leonakoester <looneylo33@gmailcom>
2025-05-08 15:26:34 +0200

    Verbesserungen

--- coordinates_memory.py
+++ coordinates_memory.py
@@ -4,8 +4,11 @@ class CoordinateBuffer:
     def __init__(self, max_length=50):
         self.buffer = deque(maxlen=max_length)
 
-    def add(self, x, y):
-        self.buffer.append((x, y))
+    def add(self, x : float, y : float):
+        if self._is_valid(x) and self._is_valid(y):
+            self.buffer.append((x, y))
+        else:
+            print("Invalid coordinate")
 
     def last_three(self):
         return self.buffer[-3:] if self.buffer else None
@@ -16,3 +19,6 @@ class CoordinateBuffer:
     def clear(self):
         self.buffer.clear()
 
+    @staticmethod
+    def _is_valid(value):
+        return isinstance(value, (int, float)) and not math.isnan(value)
