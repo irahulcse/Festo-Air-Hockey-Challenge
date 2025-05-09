7ab8b486973c372ed6aa4fdd2a7f1d6422e61c8d
leonakoester <looneylo33@gmailcom>
2025-05-09 10:27:23 +0200

    main fixed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import numpy as np
+
+from coordinates import coordinates_transformation
 from coordinates.coordinates_memory import CoordinateBuffer
-from load_camera_action import load_camera_action
 from steuerung.detect import detect_attack_from_opponent
 import time
 import cv2 as cv
@@ -9,22 +11,60 @@ def main():
     memory = CoordinateBuffer()
  #   controller = CoreControl()
 
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
     while True:
-        # Bild verarbeiten und ggf. Koordinaten in memory ablegen
-        memory = load_camera_action(memory)
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-        # Angriff erkennen
-        if detect_attack_from_opponent(memory):
-            print("Angriff erkannt!")
-            time.sleep(0.03)  # 30–40 FPS Ziel
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        gray_no_noise = cv.GaussianBlur(gray, (9, 9), 2)
 
-            # Reaktionslogik: Blocken?
-            puck_pos = memory.latest(1)
-#                if puck_pos:
-#                    intercept, direction, t = controller.find_reachable_intercept_point(puck_pos[0])
-#                    if intercept:
-#                        print(f"Abwehr bei {intercept} in {t:.2f}s")
-                    # UDP senden
+        rows = gray.shape[0]
+        circles = cv.HoughCircles(gray_no_noise, cv.HOUGH_GRADIENT, 1, rows / 8,
+                                  param1=100, param2=30,
+                                  minRadius=30, maxRadius=40)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                center = (i[0], i[1])
+                print(center)
+                robot_x, robot_y = coordinates_transformation.calc_robot_coordinates(center[0], center[1])
+                print(robot_x, robot_y)
+                memory.add(robot_x, robot_y)
+                # circle center
+                cv.circle(gray, center, 1, (0, 100, 100), 3)
+                # circle outline
+                radius = i[2]
+                cv.circle(gray, center, radius, (255, 0, 255), 3)
+                # Angriff erkennen
+                if detect_attack_from_opponent(memory):
+                    print("Angriff erkannt!")
+                    time.sleep(3)  # 30–40 FPS Ziel
+
+                    # Reaktionslogik: Blocken?
+                    puck_pos = memory.latest(1)
+            #                if puck_pos:
+            #                    intercept, direction, t = controller.find_reachable_intercept_point(puck_pos[0])
+            #                    if intercept:
+            #                        print(f"Abwehr bei {intercept} in {t:.2f}s")
+            # UDP senden
+
+        # Display the resulting frame
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
 
 
 
--- steuerung/detect.py
+++ steuerung/detect.py
@@ -13,12 +13,8 @@ def detect_attack_from_opponent(memory: CoordinateBuffer):
     x_array = three_latest_points[:,0]
     y_array = three_latest_points[:,1]
 
-    x = x_array[:, 0]
-
-    dx1 = x[1] - x[0]
-    dx2 = x[2] - x[1]
-
-    if dx1 < -5 and dx2 < -5:
+    # Richtungsänderung in x-Richtung
+    if np.sign((x_array[0] - x_array[1])) < np.sign((x_array[1] - x_array[2])):
         return True
 
     return False
